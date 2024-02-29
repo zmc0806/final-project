08164fde0693d152db274c8a036bd6f5d6673291
zmc0806 <129798536+zmc0806@users.noreply.github.com>
2024-02-29 15:09:29 -0800

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,24 @@
 <script>
-  import App from '../components/App.svelte';
+  import { onMount } from 'svelte';
+  import * as d3 from 'd3';
+
+  let svg;
+
+  onMount(() => {
+    const width = 500;
+    const height = 500;
+    const projection = d3.geoOrthographic().scale(249).translate([width / 2, height / 2]);
+    const path = d3.geoPath().projection(projection);
+    const graticule = d3.geoGraticule();
+
+    d3.select(svg)
+      .append('path')
+      .datum({ type: 'Sphere' })
+      .attr('class', 'sphere')
+      .attr('d', path);
+
+    // Add more D3 globe setup and rendering code here
+  });
 </script>
 
-<App />
+<svg bind:this={svg} width="500" height="500"></svg>
